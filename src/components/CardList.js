82707b4fcd6cd569c2927447fb5448018c326994
Muhammad Asghar Ali <51094535+alikhan137@users.noreply.github.com>
2021-09-30 09:59:34 -0700

    Add files via upload

---
+++ src/components/CardList.js
@@ -0,0 +1,23 @@
+import Card from './Card';
+import React from 'react';
+
+const CardList = ({ robots }) => {
+  return (
+    <div>
+      {
+        robots.map((user, i) => {
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+              />
+          );
+        })
+      }
+    </div>
+  );
+}
+
+export default CardList;
